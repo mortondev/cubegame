1a6486859dbb0f1940354ad653ae3691600ec052
James Morton <james@mortondev.com>
2015-10-12 21:20:46 +0100

    Added block and chunk classes.

---
+++ Assets/Block.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets
+{
+    public class Block
+    {
+        public Block()
+        {
+            Texture = new Texture2D(1, 1, TextureFormat.ARGB32, false);
+
+            // set the pixel values
+            Texture.SetPixel(0, 0,  new Color(1.0f, 1.0f, 1.0f, 0.5f));
+            Texture.SetPixel(1, 0, Color.clear);
+            Texture.SetPixel(0, 1, Color.white);
+            Texture.SetPixel(1, 1, Color.black);
+
+            // Apply all SetPixel calls
+            Texture.Apply();
+        }
+        public Texture2D Texture { get; set; }
+    }
+
+    public class BlockMesh
+    {
+        
+    }
+}
---
+++ Assets/Chunk.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets;
+
+public class Chunk : MonoBehaviour
+{
+    private Block[ , , ] _blocks;
+    public static int ChunkSize = 16;
+
+	// Use this for initialization
+	void Start () {
+
+        _blocks = new Block[ChunkSize, ChunkSize, ChunkSize];
+
+        for (var x = 0; x < ChunkSize; x++)
+        {
+            for (var y = 0; y < ChunkSize; y++)
+            {
+                for (var z = 0; z < ChunkSize; z++)
+                {
+                    _blocks[x, y, z] = new Block();
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
