775cd29dc9efddcedfd02a64606626d1c6494001
James Morton <james@mortondev.com>
2015-10-15 22:00:11 +0100

    Added Equals and GetHashCode implementations to WorldPos

--- Assets/WorldPos.cs
+++ Assets/WorldPos.cs
@@ -7,13 +7,35 @@ namespace Assets
 {
     public struct WorldPos
     {
-        public int x, y, z;
+        public readonly int X;
+        public readonly int Y;
+        public readonly int Z;
 
         public WorldPos(int x, int y, int z)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            return ((WorldPos)obj).X == X && ((WorldPos)obj).Y == Y;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var result = 0;
+                result = (result * 397) + X;
+                result = (result * 397) + Y;
+                result = (result * 397) + Z;
+                return result;
+            }
         }
     }
 }
