6aff81822647af4bfc7e1a7f0bddadc9289805f0
James Morton <james@mortondev.com>
2015-10-15 22:34:59 +0100

    Added fly camera and BlockGrass

---
+++ Assets/Blocks/BlockGrass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Blocks
+{
+    public class BlockGrass : Block
+    {
+        public override bool IsSolid()
+        {
+            return true;
+        }
+    }
+}
